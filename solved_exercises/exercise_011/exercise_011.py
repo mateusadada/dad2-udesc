9e05c9050b6670e7d5201525042f1889862d69ba
Mateus Adada <mateusadada@gmail.com>
2024-03-24 21:29:53 -0300

    Update exercise_011.py
    Improve syntax

--- solved_exercises/exercise_011/exercise_011.py
+++ solved_exercises/exercise_011/exercise_011.py
@@ -1,3 +1,3 @@
 # Suponha que seja dado o seguinte conjunto de chaves a serem inseridas em uma tabela de dispersão com 11 posições:
-# 113 , 117 , 97 , 100 , 114 , 108 , 116 , 105 , 99. Apresente o conteúdo da tabela depois de todas as chaves serem inseridas
+# 113, 117, 97, 100, 114, 108, 116, 105 e 99. Apresente o conteúdo da tabela depois de todas as chaves serem inseridas
 # com sondagem linear.
