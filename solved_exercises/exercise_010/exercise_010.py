f4747e20c2aaf3021f6f85662c08541a0f0f7a8b
Mateus Adada <mateusadada@gmail.com>
2024-03-24 21:22:26 -0300

    Update exercise_010.py
    Added the code

--- solved_exercises/exercise_010/exercise_010.py
+++ solved_exercises/exercise_010/exercise_010.py
@@ -1,1 +1,4 @@
 # Na tabela de dispersão de tamanho 13, para quais entradas as chaves 27 e 30 seriam mapeadas?
+
+print('Resposta:')
+print('A chave 27 seria mapeada para a posição 1, pois 27 % 13 = 1. Já a chave 30 seria mapeada para a posição 4, pois 30 % 13 = 4.')
